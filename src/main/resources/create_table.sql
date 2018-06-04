b9c2f5fefe176a1286d48d7475aaaf9de0f163bd
NellySmithGirl <1394052520@qq.com>
2018-06-04 19:41:48 +0800

    提交

--- src/main/resources/create_table.sql
+++ src/main/resources/create_table.sql
@@ -1,3 +1,4 @@
+drop table if exists terminal;
 CREATE TABLE `terminal` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `terminal_mac` char(18) DEFAULT NULL COMMENT '终端唯一标识',
@@ -8,3 +9,18 @@ CREATE TABLE `terminal` (
   `update_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='终端';
+
+drop table if exists `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` varchar(255) DEFAULT NULL,
+  `pass_word` char(32) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(11) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `qq` varchar(255) DEFAULT NULL,
+  `we_chat` varchar(255) DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
